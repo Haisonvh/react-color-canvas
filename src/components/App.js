0c8acecaab278e14c2f2b255473b8a08fd8dbc01
HoangHaiSonVu <haisonvh@gmail.com>
2020-10-10 14:01:05 +1100

    init project

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../css/App.css';
+import Appbar from './Appbar';
+
+function App() {
+  console.log("start");
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+
+
+export default App;
